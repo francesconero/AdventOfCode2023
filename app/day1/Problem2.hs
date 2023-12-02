b34d63d8cfdac1e591f4c87350083323e764ee26
Francesco Nero <nerofrancesco@hotmail.com>
2023-12-02 12:39:49 +0000

    Simplify last problem solution

--- app/day1/Problem2.hs
+++ app/day1/Problem2.hs
@@ -1,72 +1,77 @@
-import System.Environment (getArgs)
-import System.IO  
 import Data.Char (isDigit)
-import qualified Data.Text as T
-
-filterNumbers :: String -> String
-filterNumbers = filter isDigit
+import Data.Foldable (foldl')
+import System.Environment (getArgs)
+import System.IO
 
 keepHeadAndTail :: String -> String
 keepHeadAndTail [] = []
 keepHeadAndTail [x] = [x, x]
-keepHeadAndTail (x:xs) = [x, lastChar xs]
+keepHeadAndTail (x : xs) = [x, lastChar xs]
   where
     lastChar [y] = y
-    lastChar (_:ys) = lastChar ys
+    lastChar (_ : ys) = lastChar ys
 
 convertToInteger :: String -> Integer
 convertToInteger = read
 
 foldFunction :: (String, String) -> Char -> (String, String)
 foldFunction (output, possibleMatch) char = case possibleMatch ++ [char] of
-    "one" -> (output ++ "1", "e")
-    "two" -> (output ++ "2", "o")
-    "three" -> (output ++ "3", "e")
-    "four" -> (output ++ "4", "")
-    "five" -> (output ++ "5", "e")
-    "six" -> (output ++ "6", "")
-    "seven" -> (output ++ "7", "n")
-    "eight" -> (output ++ "8", "t")
-    "nine" -> (output ++ "9", "e")
-    "o" -> (output, possibleMatch ++ [char])
-    "on" -> (output, possibleMatch ++ [char])
-    "t" -> (output, possibleMatch ++ [char])
-    "tw" -> (output, possibleMatch ++ [char])
-    "th" -> (output, possibleMatch ++ [char])
-    "thr" -> (output, possibleMatch ++ [char])
-    "thre" -> (output, possibleMatch ++ [char])
-    "f" -> (output, possibleMatch ++ [char])
-    "fo" -> (output, possibleMatch ++ [char])
-    "fou" -> (output, possibleMatch ++ [char])
-    "fi" -> (output, possibleMatch ++ [char])
-    "fiv" -> (output, possibleMatch ++ [char])
-    "s" -> (output, possibleMatch ++ [char])
-    "si" -> (output, possibleMatch ++ [char])
-    "se" -> (output, possibleMatch ++ [char])
-    "sev" -> (output, possibleMatch ++ [char])
-    "seve" -> (output, possibleMatch ++ [char])
-    "e" -> (output, possibleMatch ++ [char])
-    "ei" -> (output, possibleMatch ++ [char])
-    "eig" -> (output, possibleMatch ++ [char])
-    "eigh" -> (output, possibleMatch ++ [char])
-    "n" -> (output, possibleMatch ++ [char])
-    "ni" -> (output, possibleMatch ++ [char])
-    "nin" -> (output, possibleMatch ++ [char])
-    x:xs -> foldl foldFunction (output ++ [x], "") xs
+  "1" -> (output ++ "1", "")
+  "2" -> (output ++ "2", "")
+  "3" -> (output ++ "3", "")
+  "4" -> (output ++ "4", "")
+  "5" -> (output ++ "5", "")
+  "6" -> (output ++ "6", "")
+  "7" -> (output ++ "7", "")
+  "8" -> (output ++ "8", "")
+  "9" -> (output ++ "9", "")
+  "one" -> (output ++ "1", "e")
+  "two" -> (output ++ "2", "o")
+  "three" -> (output ++ "3", "e")
+  "four" -> (output ++ "4", "")
+  "five" -> (output ++ "5", "e")
+  "six" -> (output ++ "6", "")
+  "seven" -> (output ++ "7", "n")
+  "eight" -> (output ++ "8", "t")
+  "nine" -> (output ++ "9", "e")
+  "o" -> (output, "o")
+  "on" -> (output, "on")
+  "t" -> (output, "t")
+  "tw" -> (output, "tw")
+  "th" -> (output, "th")
+  "thr" -> (output, "thr")
+  "thre" -> (output, "thre")
+  "f" -> (output, "f")
+  "fo" -> (output, "fo")
+  "fou" -> (output, "fou")
+  "fi" -> (output, "fi")
+  "fiv" -> (output, "fiv")
+  "s" -> (output, "s")
+  "si" -> (output, "si")
+  "se" -> (output, "se")
+  "sev" -> (output, "sev")
+  "seve" -> (output, "seve")
+  "e" -> (output, "e")
+  "ei" -> (output, "ei")
+  "eig" -> (output, "eig")
+  "eigh" -> (output, "eigh")
+  "n" -> (output, "n")
+  "ni" -> (output, "ni")
+  "nin" -> (output, "nin")
+  _ : xs -> foldl' foldFunction (output, "") xs
 
 replaceTextNumbers :: String -> String
-replaceTextNumbers input = fst $ foldl foldFunction ("", "") input
+replaceTextNumbers input = fst $ foldl' foldFunction ("", "") input
 
 processLine :: String -> Integer
-processLine = convertToInteger . keepHeadAndTail . filterNumbers . replaceTextNumbers
-
+processLine = convertToInteger . keepHeadAndTail . replaceTextNumbers
 
 main :: IO ()
 main = do
-    args <- getArgs
-    case args of
-        [filePath] -> do
-            contents <- readFile filePath
-            let processedLines = map processLine $ lines contents
-            print $ sum processedLines
-        _ -> putStrLn "Usage: stack run -- ./input_file"
+  args <- getArgs
+  case args of
+    [filePath] -> do
+      contents <- readFile filePath
+      let processedLines = map processLine $ lines contents
+      print $ sum processedLines
+    _ -> putStrLn "Usage: stack run -- ./input_file"
